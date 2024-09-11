49b252adc5a91c60d3c3590aab3e5f9814aecdfa
MuhammadSalmanAziz <m.salmanaziz12@gmail.com>
2024-09-11 23:51:28 +0500

    Working on Data Transformation

--- app.py
+++ app.py
@@ -3,12 +3,15 @@ from src.mlproject.logger import logging
 from src.mlproject.exception import CustomException
 from src.mlproject.components.data_ingestion import DataIngestion
 from src.mlproject.components.data_ingestion import DataIngestionConfig
+from src.mlproject.components.data_transformation import DataTransformationConfig,DataTransformation
 
 if __name__ == "__main__":
     logging.info("The execution has started")
     try:
         data_ingestion = DataIngestion()
         data_ingestion.initiate_data_ingestion()
+        data_transformation = DataTransformation()
+        data_transformation.initiate_data_transformation()
     except Exception as e:
         logging.info("Custom Exception")
         raise CustomException(e,sys)
