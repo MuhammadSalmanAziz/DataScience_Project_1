16e41cc64bf7637922809f04caa1929424600e47
MuhammadSalmanAziz <m.salmanaziz12@gmail.com>
2024-09-12 22:55:23 +0500

    Mlflow integrated

--- src/mlproject/components/model_trainer.py
+++ src/mlproject/components/model_trainer.py
@@ -2,6 +2,14 @@ import sys
 import os
 from dataclasses import dataclass
 
+from urllib.parse import urlparse
+import mlflow
+import mlflow.sklearn
+import dagshub
+dagshub.init(repo_owner='m.salmanaziz12', repo_name='DataScience_Project_1', mlflow=True)
+
+
+import numpy as np
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
@@ -25,6 +33,14 @@ class ModelTrainerConfig:
 class ModelTrainer:
     def __init__(self) -> None:
         self.model_trainer_config = ModelTrainerConfig()
+
+    def eval_metrics(self,actual, pred):
+        rmse = np.sqrt(mean_squared_error(actual, pred))
+        mae = mean_absolute_error(actual, pred)
+        r2 = r2_score(actual, pred)
+        return rmse, mae, r2
+    
+
     def initiate_model_trainer(self,train_df_arr,test_df_arr):
         try:
             logging.info("Split taining and testing data")
@@ -93,8 +109,46 @@ class ModelTrainer:
             best_model = models[best_model_name]
 
 
-            #print("This is the best model:")
-            #print(best_model_name)
+            print("This is the best model:")
+            print(best_model_name)
+
+            model_names = list(params.keys())
+            actual_model = ''
+            for model in model_names:
+                if  best_model_name == model:
+                    actual_model = actual_model + model
+
+            best_params = params[actual_model]
+            
+            mlflow.set_registry_uri("https://dagshub.com/m.salmanaziz12/DataScience_Project_1.mlflow")
+            tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+
+            # mlflow
+
+            with mlflow.start_run():
+
+                predicted_qualities = best_model.predict(X_test)
+
+                (rmse, mae, r2) = self.eval_metrics(y_test, predicted_qualities)
+
+                mlflow.log_params(best_params)
+
+                mlflow.log_metric("rmse", rmse)
+                mlflow.log_metric("r2", r2)
+                mlflow.log_metric("mae", mae)
+
+
+                # Model registry does not work with file store
+                if tracking_url_type_store != "file":
+
+                    # Register the model
+                    # There are other ways to use the Model Registry, which depends on the use case,
+                    # please refer to the doc for more information:
+                    # https://mlflow.org/docs/latest/model-registry.html#api-workflow
+                    mlflow.sklearn.log_model(best_model, "model", registered_model_name=actual_model)
+                else:
+                    mlflow.sklearn.log_model(best_model, "model")
+
 
             if best_model_score<0.6:
                 raise CustomException("No best model found")
